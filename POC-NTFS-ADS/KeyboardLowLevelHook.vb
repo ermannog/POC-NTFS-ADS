ab0d23c102c4882d528e7be90def1fc498ce8df0
Ermanno Goletto <e.goletto@outlook.it>
2025-08-17 23:58:40 +0200

    Third sync

---
+++ POC-NTFS-ADS/KeyboardLowLevelHook.vb
@@ -0,0 +1,80 @@
+﻿Public Class KeyboardLowLevelHook
+    Inherits WindowsHook
+
+#Region "Gestione Keyboard Hook"
+    Public Enum KeyboardMessage As Integer
+        WM_KEYDOWN = &H100
+        WM_KEYUP = &H101
+        WM_SYSKEYDOWN = &H104
+        WM_SYSKEYUP = &H105
+    End Enum
+
+    <System.Runtime.InteropServices.StructLayout(Runtime.InteropServices.LayoutKind.Sequential)>
+    Public Structure KBDLLHOOKSTRUCT
+        Public vkCode As System.Windows.Forms.Keys
+        Public scanCode As Integer
+        Public flags As Integer
+        Public time As Integer
+        Public dwExtraInfo As Integer
+
+        Public Enum LowLevelKeyboardFlags As Integer
+            LLKHF_EXTENDED = &H1
+            LLKHF_INJECTED = &H10
+            LLKHF_ALTDOWN = &H20
+            LLKHF_UP = &H80
+        End Enum
+
+        Public Function TestFlag(ByVal flag As LowLevelKeyboardFlags) As Boolean
+            Return (flags And flag) > 0
+        End Function
+    End Structure
+
+    Private Delegate Function LowLevelKeyboardProcDelegate(ByVal code As Integer,
+        ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+#End Region
+
+#Region "Gestione Keyboard Callback"
+    Public Delegate Function KeyboardDelegate(ByVal message As KeyboardLowLevelHook.KeyboardMessage,
+        ByVal keyboardInputInfo As KeyboardLowLevelHook.KBDLLHOOKSTRUCT) As Boolean
+
+    Private m_KeyboardCallback As KeyboardLowLevelHook.KeyboardDelegate
+#End Region
+
+    Public Sub New(ByVal keyboardCallback As KeyboardLowLevelHook.KeyboardDelegate)
+        'Impostazione KeyboardCallback
+        Me.m_KeyboardCallback = keyboardCallback
+    End Sub
+
+    Public Overrides Sub SetHook()
+        MyBase.SetWindowsHook(WindowsHook.HookType.WH_KEYBOARD_LL,
+            New KeyboardLowLevelHook.LowLevelKeyboardProcDelegate(AddressOf Me.LowLevelKeyboardProc),
+            False)
+    End Sub
+
+    Public Overrides Function Unhook() As Boolean
+        Return MyBase.UnhookWindowsHook()
+    End Function
+
+    Private Function LowLevelKeyboardProc(ByVal code As Integer, ByVal wParam As System.IntPtr, ByVal lParam As System.IntPtr) As Integer
+        'Verifica necessità processamento messaggio
+        If code < 0 Then Return MyBase.CallNextHook(code, wParam, lParam, False)
+
+        'Legge la Struttura KBDLLHOOKSTRUCT
+        Dim keyboardInputInfo As New KeyboardLowLevelHook.KBDLLHOOKSTRUCT
+
+        keyboardInputInfo = CType(
+            System.Runtime.InteropServices.Marshal.PtrToStructure(
+                lParam, keyboardInputInfo.GetType()),
+            KeyboardLowLevelHook.KBDLLHOOKSTRUCT)
+
+        'Invoca la KeyboardCallBack
+        Dim processed As Boolean
+        processed = Me.m_KeyboardCallback.Invoke(
+            CType(wParam.ToInt32, KeyboardLowLevelHook.KeyboardMessage),
+            keyboardInputInfo)
+
+        'Gestione valore di ritorno
+        Return MyBase.CallNextHook(code, wParam, lParam, processed)
+    End Function
+
+End Class
---
+++ POC-NTFS-ADS/WindowsHook.vb
@@ -0,0 +1,91 @@
+﻿Public MustInherit Class WindowsHook
+
+#Region "Win32Api"
+    Protected Enum HookType
+        WH_JOURNALRECORD = 0
+        WH_JOURNALPLAYBACK = 1
+        WH_KEYBOARD = 2
+        WH_GETMESSAGE = 3
+        WH_CALLWNDPROC = 4
+        WH_CBT = 5
+        WH_SYSMSGFILTER = 6
+        WH_MOUSE = 7
+        WH_HARDWARE = 8
+        WH_DEBUG = 9
+        WH_SHELL = 10
+        WH_FOREGROUNDIDLE = 11
+        WH_CALLWNDPROCRET = 12
+        WH_KEYBOARD_LL = 13
+        WH_MOUSE_LL = 14
+    End Enum
+
+    <System.Runtime.InteropServices.DllImport("user32.dll", CharSet:=Runtime.InteropServices.CharSet.Auto)>
+    Private Shared Function SetWindowsHookEx(
+        ByVal idHook As HookType,
+        ByVal lpfn As System.Delegate,
+        ByVal hMod As IntPtr,
+        ByVal dwThreadId As Integer) As IntPtr
+    End Function
+
+    <System.Runtime.InteropServices.DllImport("user32.dll", CharSet:=Runtime.InteropServices.CharSet.Auto)>
+    Private Shared Function UnhookWindowsHookEx(ByVal hHook As System.IntPtr) As Boolean
+    End Function
+
+    <System.Runtime.InteropServices.DllImport("user32.dll", CharSet:=Runtime.InteropServices.CharSet.Auto)>
+    Private Shared Function CallNextHookEx _
+             (ByVal hhk As IntPtr, ByVal nCode As Integer,
+              ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+    End Function
+#End Region
+
+    <System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.FunctionPtr)>
+    Private m_HookCallback As System.Delegate
+
+    Private m_HookCallbackHandle As System.IntPtr = System.IntPtr.Zero
+
+    Protected Overridable Function SetWindowsHook(ByVal hookType As WindowsHook.HookType, ByVal hookCallback As System.Delegate, ByVal onlyCurrentThread As Boolean) As System.IntPtr
+        If Not Me.m_HookCallback Is Nothing Then Me.UnhookWindowsHook()
+
+        'Impostazione Hook Callback
+        Me.m_HookCallback = hookCallback
+
+        'Modulo a cui appartiene la HookCallBack
+        Dim moduleHandle As System.IntPtr
+        moduleHandle = System.Runtime.InteropServices.Marshal.GetHINSTANCE(
+            Me.m_HookCallback.GetType.Module)
+
+        'Aggiunta Hook
+        If onlyCurrentThread Then
+            Me.m_HookCallbackHandle = WindowsHook.SetWindowsHookEx(hookType,
+                Me.m_HookCallback, System.IntPtr.Zero, System.Threading.Thread.CurrentThread.ManagedThreadId)
+        Else
+            Me.m_HookCallbackHandle = WindowsHook.SetWindowsHookEx(hookType,
+                Me.m_HookCallback, moduleHandle, 0)
+        End If
+    End Function
+
+    Protected Overridable Function UnhookWindowsHook() As Boolean
+        Dim returnValue As Boolean
+        returnValue = WindowsHook.UnhookWindowsHookEx(Me.m_HookCallbackHandle)
+
+        Me.m_HookCallback = Nothing
+        Me.m_HookCallbackHandle = System.IntPtr.Zero
+
+        Return returnValue
+    End Function
+
+    Protected Overridable Function CallNextHook(ByVal code As Integer,
+              ByVal wParam As IntPtr, ByVal lParam As IntPtr,
+              ByVal processed As Boolean) As Integer
+
+        If processed Then
+            Return 1
+        Else
+            Return WindowsHook.CallNextHookEx(Me.m_HookCallbackHandle, code, wParam, lParam)
+        End If
+    End Function
+
+    Public MustOverride Sub SetHook()
+
+    Public MustOverride Function Unhook() As Boolean
+End Class
